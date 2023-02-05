280289a6f0a7512fa658ee7e410f5ce4b58746e7
Ben <Ben@DESKTOP-LLVB17J>
2023-02-05 14:18:11 -0600

    Add project files.

---
+++ Module.cs
@@ -0,0 +1,65 @@
+﻿using Blish_HUD;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Microsoft.Xna.Framework;
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Falson.Squad_Role_Randomizer
+{
+    [Export(typeof(Blish_HUD.Modules.Module))]
+    public class Module : Blish_HUD.Modules.Module
+    {
+
+        private static readonly Logger Logger = Logger.GetLogger<Module>();
+
+        #region Service Managers
+        internal SettingsManager SettingsManager => this.ModuleParameters.SettingsManager;
+        internal ContentsManager ContentsManager => this.ModuleParameters.ContentsManager;
+        internal DirectoriesManager DirectoriesManager => this.ModuleParameters.DirectoriesManager;
+        internal Gw2ApiManager Gw2ApiManager => this.ModuleParameters.Gw2ApiManager;
+        #endregion
+
+        [ImportingConstructor]
+        public Module([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) { }
+
+        protected override void DefineSettings(SettingCollection settings)
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+
+        }
+
+        protected override async Task LoadAsync()
+        {
+
+        }
+
+        protected override void OnModuleLoaded(EventArgs e)
+        {
+
+            // Base handler must be called
+            base.OnModuleLoaded(e);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Unload()
+        {
+            // Unload here
+
+            // All static members must be manually unset
+        }
+
+    }
+
+}
